ebcb956ae0397016ac8d30008b9e400786b82e3f
Rob Puncel <rpuncel@gmail.com>
2024-05-31 12:21:45 -0700

    add intensity emojis to section list

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { ActionPanel, Detail, List, Action, getPreferenceValues } from "@raycast/api";
 import { useEffect, useState } from "react";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 interface Effort {
   title: string;
@@ -13,53 +13,69 @@ interface Preferences {
   effortsFolder: string;
 }
 
-const intensities = ["On", "Ongoing", "Simmering"]
+const intensities = [
+  {
+    name: "On",
+    icon: "🔥",
+  },
+  {
+    name: "Ongoing",
+    icon: "♻️",
+  },
+  {
+    name: "Simmering",
+    icon: "〰︎",
+  },
+];
 
 function getDirectories(pathStr: string) {
-  const efforts = new Map<string, Effort[]>()
-  intensities.forEach((intensity) => {
-    const dir = path.join(pathStr, intensity)
-    console.log("looking in dir: ", dir)
-    const files = fs.readdirSync(dir).filter(function (file) {
-      const isFile = fs.statSync(path.join(dir, file)).isFile();
-      console.log(`file: ${file} isFile: ${isFile}`)
-      return isFile && file.endsWith('.md');
-      }).map((file) => {
-        return { title: file, intensity: intensity, path: path.join(dir, file) }
-      });
-    efforts.set(intensity, files)
-  });
-  return efforts
-     
+  const efforts = new Map<string, Effort[]>();
+  intensities
+    .map((intensity) => intensity.name)
+    .forEach((intensity) => {
+      const dir = path.join(pathStr, intensity);
+      console.log("looking in dir: ", dir);
+      const files = fs
+        .readdirSync(dir)
+        .filter(function (file) {
+          const isFile = fs.statSync(path.join(dir, file)).isFile();
+          console.log(`file: ${file} isFile: ${isFile}`);
+          return isFile && file.endsWith(".md");
+        })
+        .map((file) => {
+          return { title: file, intensity: intensity, path: path.join(dir, file) };
+        });
+      efforts.set(intensity, files);
+    });
+  return efforts;
 }
 
 export default function Command() {
   const preferences = getPreferenceValues<Preferences>();
-  const [ efforts, setEfforts ] = useState<Map<string, Effort[]>>();
+  const [efforts, setEfforts] = useState<Map<string, Effort[]>>();
 
   useEffect(() => {
     const efforts = getDirectories(preferences.effortsFolder);
-    setEfforts(efforts)
+    setEfforts(efforts);
   }, []);
 
   return (
     <List isLoading={efforts === undefined}>
       {intensities.map((intensity) => (
-        <List.Section title={intensity}>
-          {efforts?.get(intensity)?.map((effort, index) => (
+        <List.Section title={`${intensity.icon} ${intensity.name}`}>
+          {efforts?.get(intensity.name)?.map((effort, index) => (
             <List.Item
               key={index}
-              title={effort.title} 
+              title={effort.title}
               actions={
                 <ActionPanel>
-                  <Action.Open title="Open in Obsidian" target={`obsidian://open?path=${effort.path}`}/>
+                  <Action.Open title="Open in Obsidian" target={`obsidian://open?path=${effort.path}`} />
                 </ActionPanel>
               }
             />
           ))}
         </List.Section>
       ))}
-    
     </List>
   );
 }
