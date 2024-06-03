3dddcfdd8d3e24003fbc333cbf7b6627b19a167d
Rob Puncel <rpuncel@gmail.com>
2024-06-03 15:22:32 -0700

    add action for creating the new effort

--- src/index.tsx
+++ src/index.tsx
@@ -52,10 +52,17 @@ function getDirectories(pathStr: string) {
 
 function NewEffortForm() {
   const intensityItems = intensities.map((intensity) => (
-    <Form.Dropdown.Item value={intensity.name} title={intensity.name} />
+    <Form.Dropdown.Item key={intensity.name} value={intensity.name} title={intensity.name} />
   ));
   return (
-    <Form navigationTitle="Create a new effort">
+    <Form
+      navigationTitle="Create a new effort"
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Submit New Effort" onSubmit={(values) => console.log(values)} />
+        </ActionPanel>
+      }
+    >
       <Form.Dropdown id="intensity" title="Intensity">
         {intensityItems}
       </Form.Dropdown>
