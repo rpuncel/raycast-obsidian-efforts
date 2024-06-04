2a1e9c8c2f7cb970b022e4d94a8ea2b2b7972c92
Rob Puncel <rpuncel@gmail.com>
2024-06-03 17:05:31 -0700

    add title field to new effort form

--- src/components/NewEffortForm.tsx
+++ src/components/NewEffortForm.tsx
@@ -1,7 +1,23 @@
 import { Form, ActionPanel, Action } from "@raycast/api";
 import { intensities } from "../globals";
+import { useState } from "react";
 
 export function NewEffortForm() {
+  const [titleError, setTitleError] = useState<string | undefined>();
+  function dropTitleErrorIfNeeded() {
+    if (titleError && titleError.length > 0) {
+      setTitleError(undefined);
+    }
+  }
+  const validateTitle = (title: string | undefined) => {
+    console.log(title);
+    if (!title || title?.length == 0) {
+      setTitleError("The field can't be empty.");
+      return;
+    }
+    dropTitleErrorIfNeeded();
+  };
+
   const intensityItems = intensities.map((intensity) => (
     <Form.Dropdown.Item key={intensity.name} value={intensity.name} title={intensity.name} />
   ));
@@ -14,6 +30,14 @@ export function NewEffortForm() {
         </ActionPanel>
       }
     >
+      <Form.TextField
+        id="title"
+        title="Title"
+        placeholder="Enter the title of the effort"
+        error={titleError}
+        onChange={dropTitleErrorIfNeeded}
+        onBlur={(event) => validateTitle(event.target.value)}
+      />
       <Form.Dropdown id="intensity" title="Intensity">
         {intensityItems}
       </Form.Dropdown>
